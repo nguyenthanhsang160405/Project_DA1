21b48c37f505ca9592f8dcba52768c8865dfe2bb
tranhuunghi <huunghitc1@gmail.com>
2024-11-09 00:00:37 +0700

    nghiupdate1dong

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,22 @@
-var a = 0;
-function truot(){
-    hinh = document.getElementsByName('hinh');
-    anh = document.getElementById('anh');
-    var rong = hinh[0].offsetWidth;
-    a++;
-    if (a == hinh.length){
-        a = 0;
-    }
-    anh.style.transform = `translateX(${-rong * a}px)`;
-    anh.style.transition = `0.5s ease-in-out`;
+// var a = 0;
+// function truot(){
+//     hinh = document.getElementsByName('hinh');
+//     anh = document.getElementById('anh');
+//     var rong = hinh[0].offsetWidth;
+//     a++;
+//     if (a == hinh.length){
+//         a = 0;
+//     }
+//     anh.style.transform = `translateX(${-rong * a}px)`;
+//     anh.style.transition = `0.5s ease-in-out`;
+// }
+//     truot();
+//     setInterval(truot,2000);
+var arr = [`public/img/banner1.jpg`,`public/img/BANNER 2.jpg`,`public/img/banner3.jpg`];
+var i = 0;
+function slideshow(){
+    i++;
+    if(i == arr.length) i =0;
+    document.getElementById('slideshow').src = arr[i];
 }
-    truot();
-    setInterval(truot,2000);
+setInterval(slideshow,3000);
