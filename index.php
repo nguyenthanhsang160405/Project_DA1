0cb95dd2eb594aca096ad6de0ae317a7590ffc3f
ThanhSang <nguyenthanhsang160405@gmail.com>
2024-11-19 15:12:49 +0700

    updateFUllSag

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <?php
     ob_start();
     session_start();
-    
     include_once 'app/model/database.php';
     include_once 'app/model/productModel.php';
     include_once 'app/model/categoryProModel.php';
+    include_once 'app/model/blogModel.php';
+    include_once 'app/model/cateBlogModel.php';
+    include_once 'app/model/commentModel.php';
+    include_once 'app/model/detailOrderModel.php';
+    include_once 'app/model/imageProModel.php';
+    include_once 'app/model/MaillerUser.php';
+    include_once 'app/model/orderModel.php';
+    include_once 'app/model/UserModel.php';
+    include_once 'app/model/voucherModel.php';
+    
+
+
+
+
+
     
 ?>
