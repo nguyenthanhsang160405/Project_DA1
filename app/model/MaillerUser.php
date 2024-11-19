f6a3d48fe7f74b8cb2efc91e8a2fcd236ee3fe6c
ThanhSang <nguyenthanhsang160405@gmail.com>
2024-11-19 15:14:21 +0700

    SangupdateBackendUSer

--- app/model/MaillerUser.php
+++ app/model/MaillerUser.php
@@ -3,11 +3,11 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
     
-require '../PHPMailer/src/Exception.php';
-require '../PHPMailer/src/PHPMailer.php';
-require '../PHPMailer/src/SMTP.php';
+require './PHPMailer/src/Exception.php';
+require './PHPMailer/src/PHPMailer.php';
+require './PHPMailer/src/SMTP.php';
 
-    class Mailler{
+    class MaillerUser{
         public $email;
         public function __construct(){
             $this->email = new PHPMailer(true);
