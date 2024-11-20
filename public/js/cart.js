4612f79161215f2e36b4a8224371852930e29791
12aesieunhangao <tran0343517204@gmail.com>
2024-11-20 17:52:43 +0700

    hoangupdate

---
+++ public/js/cart.js
@@ -0,0 +1,29 @@
+// <!-- Điều chỉnh số lượng -->
+    let quantity = 0;
+
+   document.getElementById('increase-cout-cart').addEventListener('click', function() {
+       quantity++;
+       document.getElementById('quantity-cout-cart').textContent = quantity;
+   });
+
+   document.getElementById('decrease-cout-cart').addEventListener('click', function() {
+       if (quantity > 0) {
+           quantity--;
+           document.getElementById('quantity-cout-cart').textContent = quantity;
+       }
+   });
+
+
+   
+//    <!-- ẩn hiện ghi chú -->
+       document.getElementById('form-note').style.display = 'block';
+           shownote();
+           function shownote() {
+               var x = document.getElementById('form-note');
+               if(x.style.display == 'none'){
+                   x.style.display = 'block'
+               }else{
+                   x.style.display = 'none'
+               }
+           }
+
