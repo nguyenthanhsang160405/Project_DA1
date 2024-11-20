9507031d9ee18802e051d075e2c583aee5423b47
12aesieunhangao <tran0343517204@gmail.com>
2024-11-20 12:45:07 +0700

    hoangupdate

--- app/model/categoryProModel.php
+++ app/model/categoryProModel.php
@@ -4,7 +4,7 @@
         public function __construct(){
             $this->db = new Database();
         }
-        public function InsertCategoryPro($param): bool{
+        public function InsertCategoryPro($param){
             $sql = "INSERT INTO loai_sanpham (ten_loai,mota_loai) VALUES (?,?)";
             return $this->db->query($sql,$param);
         }
